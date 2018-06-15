bb8dce4038be2f27e1ad18434cd1f22e0c874e2a
MitkoZhelev <mitko.j.zhelev@gmail.com>
2018-06-15 11:30:32 +0300

    Done

---
+++ src/main/java/com/example/demo/BlockChainSceletonApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlockChainSceletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlockChainSceletonApplication.class, args);
+	}
+}
